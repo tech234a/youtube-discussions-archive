2d728503f83cf6687c4a633a249d17442ced3aba
coletdjnz <colethedj@protonmail.com>
2021-10-06 17:23:02 +1300

    remove unused code

---
+++ tracker.py
@@ -0,0 +1,176 @@
+from typing import Optional, List
+from enum import Enum, auto
+
+import requests
+from requests.adapters import HTTPAdapter
+from requests.packages.urllib3.util.retry import Retry
+
+from os.path import isfile
+from json import loads
+
+# https://github.com/ArchiveTeam/tencent-weibo-grab/blob/9bae5f9747e014db9227821a9c11557267967023/pipeline.py
+VERSION = "20211006.01"
+
+# TODO
+TRACKER_ID = "TODO"
+TRACKER_HOST = "trackerproxy.meo.ws"
+
+BACKFEED_HOST = "blackbird-amqp.meo.ws:23038"
+
+BACKFEED_ENDPOINT = f"http://{BACKFEED_HOST}/{TRACKER_ID}-kj57sxhhzcn2kqjp/"
+TRACKER_ENDPOINT = f"http://{TRACKER_HOST}/{TRACKER_ID}"
+
+from os import environ
+if "TRACKER_USERNAME" in environ.keys():
+    TRACKER_USERNAME = environ["TRACKER_USERNAME"]
+elif isfile("config.json"):
+    try:
+        TRACKER_USERNAME = loads(open("config.json").read())["TRACKER_USERNAME"]
+    except:
+        TRACKER_USERNAME = "Unnamed"
+else:
+    TRACKER_USERNAME = "Unnamed"
+
+
+# https://findwork.dev/blog/advanced-usage-python-requests-timeouts-retries-hooks/
+retry_strategy = Retry(
+    total=4,
+    backoff_factor=2,
+    status_forcelist=[x for x in range(500, 600)] + [429],
+    method_whitelist=["GET", "POST"]
+)
+adapter = HTTPAdapter(max_retries=retry_strategy)
+tracker_session = requests.Session()
+tracker_session.mount("https://", adapter)
+tracker_session.mount("http://", adapter)
+
+
+class ItemType(Enum):
+    Channel = auto()
+
+
+def add_item_to_tracker(item_type: ItemType, item_id: str) -> bool:
+    """Feed items into the tracker through backfeed (item names will be deduplicated):
+    # curl -d 'ITEMNAME' -so/dev/null $amqp_endpoint
+
+    # Response codes:
+    # 200 - Item added to tracker
+    # 409 - Item is already in tracker
+    # 404 - Project backfeed channel not found
+    # 400 - Item name has a bad format
+    """
+    type_name = item_type.name.lower()
+    item_name = f"{type_name}:{item_id}"
+
+    req = tracker_session.post(BACKFEED_ENDPOINT, data=item_name)
+
+    code = req.status_code
+
+    if code == 200:
+        # print(f"[INFO] Item ID \'{item_name}\' added to tracker successfully")
+        return True
+    elif code == 409:
+        # print(f"[INFO] Item ID \'{item_name}\' has already been added to tracker")
+        return True
+    elif code == 404:
+        print(f"[ERROR] Unable to add item ID \'{item_name}\' to tracker. Project backfeed channel not found: {BACKFEED_ENDPOINT}")
+    elif code == 400:
+        print(f"[ERROR] Item ID \'{item_name}\' has a bad format")
+    else:
+        print(f"[ERROR] Unknown response code adding item \'{item_name}\' to tracker: {code}")
+
+    return False
+
+
+def request_item_from_tracker() -> Optional[str]:
+
+    data = {
+        "downloader": TRACKER_USERNAME,
+        "api_version": "2",
+        "version": VERSION
+    }
+
+    req = tracker_session.post(f"{TRACKER_ENDPOINT}/request", json=data)
+
+    code = req.status_code
+
+    if code == 200:
+        data = req.json()
+
+        if "item_name" in data:
+            item_name = data["item_name"]
+            print(f"[INFO] Received an item from tracker: {item_name}")
+
+            return item_name
+        else:
+            print(f"[ERROR] Received item is missing the \'item_name\' key: {data}")
+
+    else:
+        print(f"[ERROR] Unable to get an item from tracker. Status: {code}")
+
+
+def request_upload_target() -> Optional[str]:
+    req = tracker_session.get(f"{TRACKER_ENDPOINT}/upload")
+
+    code = req.status_code
+
+    if code == 200:
+        data = req.json()
+
+        if "upload_target" in data:
+            upload_target = data["upload_target"]
+            print(f"[INFO] Received an upload target from tracker: {upload_target}")
+            return upload_target
+        else:
+            print(f"[ERROR] Response is missing the \'upload_target\' key: {data}")
+
+    else:
+        print(f"[ERROR] Unable to get an upload target from tracker. Status: {code}")
+
+
+def request_all_upload_targets() -> Optional[List[str]]:
+    req = tracker_session.get(f"{TRACKER_ENDPOINT}/upload_targets")
+
+    code = req.status_code
+
+    if code == 200:
+        data = req.json()
+        print(f"[INFO] Received all upload targets from tracker: {data}")
+        return data
+    else:
+        print(f"[ERROR] Unable to get all upload targets from tracker. Status: {code}")
+
+
+# `item_name` includes type prefix (video:id, playlist:id, etc)
+def mark_item_as_done(item_name: str, item_size_bytes: int) -> bool:
+
+    data = {
+        "downloader": TRACKER_USERNAME,
+        "version": VERSION,
+        "item": item_name,
+        "bytes": {
+            "data": item_size_bytes
+        }
+    }
+
+    req = tracker_session.post(f"{TRACKER_ENDPOINT}/done", json=data)
+
+    code = req.status_code
+
+    if code == 200:
+        print(f"[INFO] Marked item \'{item_name}\' as done")
+        return True
+    elif code > 399 and code < 500:
+        print(f"[ERROR] Unable to mark item as done. Status: {code}")
+    else:
+        print(f"[ERROR] Unknown response code while marking item \'{item_name}\' as done: {code}")
+
+    return False
+
+
+# if __name__ == "__main__":
+    # print(add_item_to_tracker(ItemType.Channel, "test10"))
+    # print(request_item_from_tracker())
+    # print(request_upload_target())
+    # print(request_all_upload_targets())
+    # print(mark_item_as_done("test4", 200))
---
+++ worker.py
@@ -0,0 +1,191 @@
+# Let's remind people who still have this running to shut it down
+from os.path import isfile
+from json import loads
+from os import environ
+import requests
+from sys import exit
+
+if "TRACKER_USERNAME" in environ.keys():
+    TRACKER_USERNAME = environ["TRACKER_USERNAME"]
+elif isfile("config.json"):
+    try:
+        TRACKER_USERNAME = loads(open("config.json").read())["TRACKER_USERNAME"]
+    except:
+        TRACKER_USERNAME = "Unnamed"
+else:
+    TRACKER_USERNAME = "Unnamed"
+
+from threading import Thread
+import requests
+from time import sleep
+from os import mkdir, rmdir, listdir, system, environ
+from os.path import isdir, isfile, getsize
+from json import loads
+
+import signal
+import tracker
+from shutil import rmtree, which
+from queue import Queue
+from gc import collect
+
+# useful Queue example: https://stackoverflow.com/a/54658363
+jobs = Queue()
+
+try:
+    mkdir("out")
+except:
+    pass
+
+try:
+    mkdir("directory")
+except:
+    pass
+
+HEROKU = False
+if isfile("../Procfile"):
+    HEROKU = True
+
+assert which("zip") and which("rsync") and which(
+    "curl"), "Please ensure the zip, rsync, and curl commands are installed on your system."
+
+
+
+# Graceful Shutdown
+class GracefulKiller:
+    kill_now = False
+
+    def __init__(self):
+        signal.signal(signal.SIGINT, self.exit_gracefully)
+        signal.signal(signal.SIGTERM, self.exit_gracefully)
+
+    def exit_gracefully(self, signum, frame):
+        print("Graceful exit process initiated, no longer accepting new tasks but finishing existing ones...")
+        self.kill_now = True
+
+
+gkiller = GracefulKiller()
+
+
+# microtasks
+def threadrunner():
+    jobs = Queue()
+    while True:
+        if not jobs.empty():
+            task, vid, args = jobs.get()
+            if task == "submitdiscovery":
+                tracker.add_item_to_tracker(args, vid)
+            elif task == "channel":
+                # TODO
+                # try:
+                #     y = ydl.extract_info("https://www.youtube.com/channel/" + desit.split(":", 1)[1], download=False)
+                #     for itemyv in y["entries"]:
+                #         jobs.put(("submitdiscovery", itemyv["id"], tracker.ItemType.Video))
+                #
+                #     # channel created playlists
+                #     y = process_channel(desit.split(":", 1)[1])
+                #     for itemyv in y["playlists"]:
+                #         jobs.put(("submitdiscovery", itemyv, tracker.ItemType.Playlist))
+                #     for itemyv in y["channels"]:
+                #         jobs.put(("submitdiscovery", itemyv, tracker.ItemType.Channel))
+                #
+                #     jobs.put(("complete", None, "channel:" + args))
+                # except:
+                #     print("YouTube-DL error, ignoring but not marking as complete...",
+                #           "https://www.youtube.com/channel/" + desit.split(":", 1)[1])
+
+            elif task == "complete":
+                size = 0
+                # TODO
+                if ":" in args:
+                    if args.split(":", 1)[0] == "channel":
+                        # check if dir is empty, make zip if needed
+                        if isdir("out/" + args.split(":", 1)[1]):
+                            if not listdir("out/" + args.split(":", 1)[1]):
+                                rmdir("out/" + args.split(":", 1)[1])
+                            else:
+                                # zip it up
+                                if not isdir("directory/" + args.split(":", 1)[1]):
+                                    mkdir("directory/" + args.split(":", 1)[1])
+
+                                while not isfile(
+                                        "directory/" + args.split(":", 1)[1] + "/" + args.split(":", 1)[1] + ".zip"):
+                                    print("Attempting to zip item...")
+                                    system("zip -9 -r -j directory/" + args.split(":", 1)[1] + "/" + args.split(":", 1)[
+                                        1] + ".zip out/" + args.split(":", 1)[1])
+
+                                # get a target
+                                targetloc = None
+                                while not targetloc:
+                                    targetloc = tracker.request_upload_target()
+                                    if targetloc:
+                                        break
+                                    else:
+                                        print("Waiting 5 minutes...")
+                                        sleep(300)
+
+                                while True:
+                                    if targetloc.startswith("rsync"):
+                                        exitinfo = system(
+                                            "rsync -rltv --timeout=300 --contimeout=300 --progress --bwlimit 0 --recursive --partial --partial-dir .rsync-tmp --min-size 1 --no-compress --compress-level 0 directory/" +
+                                            args.split(":", 1)[1] + "/ " + targetloc)
+                                    elif targetloc.startswith("http"):
+                                        exitinfo = system("curl -F " + args.split(":", 1)[1] + ".zip=@directory/" +
+                                                          args.split(":", 1)[1] + "/" + args.split(":", 1)[
+                                                              1] + ".zip " + targetloc)
+
+                                    if exitinfo == 0:  # note that on Unix this isn't necessarily the exit code but it's still 0 upon successful exit
+                                        break
+                                    else:
+                                        print("Error in sending data to target, waiting 30 seconds and trying again.")
+                                        sleep(30)
+
+                                size = getsize(
+                                    "directory/" + args.split(":", 1)[1] + "/" + args.split(":", 1)[1] + ".zip")
+                                # cleanup
+                                try:
+                                    rmtree("directory/" + args.split(":", 1)[1] + "/")
+                                    rmdir("directory/" + args.split(":", 1)[1] + "/")
+                                    rmtree("out/" + args.split(":", 1)[1] + "/")
+                                    rmdir("out/" + args.split(":", 1)[1] + "/")
+                                except:
+                                    pass
+                tracker.mark_item_as_done(args, size)
+            jobs.task_done()
+        else:
+            if not gkiller.kill_now:
+                # get a new task from tracker
+                collect()  # cleanup
+
+                desit = tracker.request_item_from_tracker()
+                print("New task:", desit)
+
+                if desit:
+                    if desit.split(":", 1)[0] == "channel":
+                        jobs.put(("channel", None, desit.split(":", 1)[1]))
+                    else:
+                        print("Ignoring item for now", desit)
+                else:
+                    print("Ignoring item for now", desit)
+            else:
+                break
+
+
+threads = []
+
+THREADCNT = 50
+if HEROKU:
+    THREADCNT = 20
+# now create the rest of the threads
+for i in range(THREADCNT):
+    runthread = Thread(target=threadrunner)
+    runthread.start()
+    threads.append(runthread)
+    del runthread
+
+# https://stackoverflow.com/a/11968881
+for x in threads:
+    x.join()
+    threads.remove(x)
+    del x
+
+print("Exiting...")
